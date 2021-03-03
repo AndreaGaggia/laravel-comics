2b7517be3557a873625018a987b2b71a2ff97bca
AndreaGaggia <gaggiandrea@gmail.com>
2021-03-03 15:11:08 +0100

    admin routes grouped

--- routes/web.php
+++ routes/web.php
@@ -19,4 +19,7 @@ Route::get('/', function () {
 
 Auth::routes(['register' => false]);
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::middleware('auth')->prefix('admin')->name('admin.')->group(function () {
+    Route::get('/', 'HomeController@index')->name('home');
+    Route::resource('comics', 'ComicController');
+});
